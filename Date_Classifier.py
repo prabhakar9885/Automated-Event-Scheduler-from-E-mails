0b16dd8c1394182e5e609cbae92eccc9799b786b
Prabhakar Bikkaneti <prabhakar.9885@gmail.com>
2016-12-15 00:24:54 +0530

    Updated the train data

--- Date_Classifier.py
+++ Date_Classifier.py
@@ -14,53 +14,60 @@ class MLStripper(HTMLParser):
     def get_data(self):
         return ''.join(self.fed)
 
-
 categories = ['No','Yes']
-mailThread = p.load(open("mailThreadenron_mail_20150507.dump", "rb") )
-
-print "Preparing the train data..."
-train = []
-label = []
-dateTimeTrain = open("./DateAndTime.txt", "r" ).readlines()
-for item in dateTimeTrain:
-	try:
-		key, val = item.split("=")
-		train += [key.strip()]
-		label += [val.strip()]
-	except:
-		print len(label)
-
-label = np.array(label)
-
-# Tokenizing text 
-# ===============
-count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform(train)
-X_train_counts.shape
 
 
 
-# From occurrences to frequencies
-# ===============================
-tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-X_train_tfidf.shape
+def buildDateClassifier():
+
+	mailThread = p.load( open("mailThreadenron_mail_20150507.dump", "rb") )
+
+	print "Preparing the train data..."
+	train = []
+	label = []
+	dateTimeTrain = open("./DateAndTime.txt", "r" ).readlines()
+	for item in dateTimeTrain:
+		try:
+			key, val = item.split("=")
+			train += [key.strip()]
+			label += [int(val.strip())]
+		except:
+			print len(label)
+
+	label = np.array(label)
+
+	# Tokenizing text 
+	# ===============
+	count_vect = CountVectorizer()
+	X_train_counts = count_vect.fit_transform(train)
+	X_train_counts.shape
+
+	# From occurrences to frequencies
+	# ===============================
+	tfidf_transformer = TfidfTransformer()
+	X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+	X_train_tfidf.shape
+
+	# Training an SVM classifier
+	print "Training the SVM..."
+	clf = SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter=5, random_state=42).fit(X_train_tfidf, label )
+
+	return clf, count_vect, tfidf_transformer
 
 
 
-# Training an SVM classifier
-print "Training the SVM..."
-clf = SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter=5, random_state=42).fit(X_train_tfidf, label )
+if __name__ == '__main__':
 
+	clf, count_vect, tfidf_transformer = buildDateClassifier()
 
-# Testing
-print "Testing the model..."
-print "Enter the Date and Time:"
-while True:
-	str = raw_input();
-	docs_new = [str]
-	X_new_counts = count_vect.transform(docs_new)
-	X_new_tfidf = tfidf_transformer.transform(X_new_counts)
+	# Testing
+	print "Testing the model..."
+	print "Enter the Date and Time:"
+	while True:
+		str = raw_input();
+		docs_new = [str]
+		X_new_counts = count_vect.transform(docs_new)
+		X_new_tfidf = tfidf_transformer.transform(X_new_counts)
 
-	predicted = clf.predict(X_new_tfidf)
-	print predicted
+		predicted = clf.predict(X_new_tfidf)
+		print predicted
--- Mail_Classifier.py
+++ Mail_Classifier.py
@@ -16,102 +16,110 @@ class MLStripper(HTMLParser):
 
 
 categories = ['No','Yes','reschedule']
-mailThread = p.load(open("mailThreadenron_mail_20150507.dump", "rb") )
-
-print "Preparing the train data..."
-
-keys = mailThread.keys()
-targets = [0]*len(keys)
-indexOfSchedulingMails = [1, 2, 8, 21, 29, 30, 37, 38, 55, 59, 61, 63, 68, 69, 74, 82, 92, 110 ,113 ,124 ,131 ,139, 161 ,171 ,194, 195, 204, 213, 223, 255, 308, 323, 340, 365, 374, 392, 399, 403 ]
-indexOfReSchedulingMails = [105, 147, 306]
-for i in indexOfSchedulingMails:
-	targets[i] = 1
-for i in indexOfReSchedulingMails:
-	targets[i] = 2
-
-allBodyMerged = []
-for key in keys:
-	mails = mailThread[key]
-	temp = "";
-	for mail in mails:
-		if 'body' in mail and len(mail['body'])!=0:
-			indx = mail['body'].index("----") if "----" in mail['body'] else -1
-			s = MLStripper()
-			s.feed( mail['body'].replace("\n","").replace("\r","").replace("\t","")[:indx] )
-			temp += s.get_data() + ". "
-	allBodyMerged += [ temp ]
-
-# Augmenting the schdeuling mails
-schdeulingMails = [ \
-		'Lets meet at 2 am', \
-		'Lets meet in the afternoon',\
-		'scheduled a meeting for tomorrow',\
-		'we will meet tomorrow',\
-		'we are meeting tomorrow',\
-		'we will meet today',\
-		'we are meeting today',\
-		'would like to meet and speak to you on the 15th of September',\
-		'The monthly meeting will be on Monday, 12th December 2011',\
-		'The meetings will be on Monday'
-		]
-for m in schdeulingMails:
-	allBodyMerged += [m]
-	targets += [1]
 
 
-# Augmenting the Reschdeuling mails
-reschdeulingMailsSamples = [\
-		 'rescheduled the meeting for tomorrow'
-		 'we have tentatively rescheduled the meeting for August 20.'
-		 'we will postpone the meet',\
-		 'we will postpone the meeting',\
-		 'we will reschedule the meet',\
-		 'we will reschedule the meeting',\
-		 'We regret to inform you the August 8 meeting has been postponed.',\
-		 'I\'m afraid I have to request to reschedule our meeting of June 15.',\
-		 'This is to inform you that the board meeting that was scheduled to happen on the 12th of September has been postponed to the 15th of September due to some official reasons.  We are sorry for the inconvenience caused and would like to meet and speak with you on the 15th of September.',\
-		 'I deeply regret that I will be unable to keep our appointment scheduled at 12: 00 noon on Wednesday, 5th March 2014. Due to some urgent circumstances in my office, I have to travel to Singapore, hence my inability to attend the aforementioned meeting.I will return on 12th March 2014 and as soon as I return, I will call your office to reschedule our meeting. Probably we can meet the next day after my return and will call your office in this regard. Once again, I apologise my inability to attend the meeting.',\
-		 'Please consider my humble request for postponing my appointment date and be kind enough to permit me to join on a future date and oblige.',\
-		 'This is to inform you that our meeting for Tuesday has to be postponed.',\
-		 'This email is to let you all know that our scheduled meeting for this afternoon has been postponed.',
-		 'could you please reschedule the meeting for tomorrow',
-		 'The monthly meeting that was to take place on Monday, 12th December 2011 has been postponed because of an urgent visit that I will be making on the project site on that day during the morning hours.'
-		]
-for m in reschdeulingMailsSamples:
-	allBodyMerged += [m]
-	targets += [2]
+def buildMailClassifier():
+
+	mailThread = p.load(open("mailThreadenron_mail_20150507.dump", "rb") )
+
+	print "Preparing the train data..."
+
+	keys = mailThread.keys()
+	targets = [0]*len(keys)
+	indexOfSchedulingMails = [1, 2, 8, 21, 29, 30, 37, 38, 55, 59, 61, 63, 68, 69, 74, 82, 92, 110 ,113 ,124 ,131 ,139, 161 ,171 ,194, 204, 213, 223, 255, 308, 323, 340, 365, 374, 392, 399, 403 ]
+	indexOfReSchedulingMails = [105, 147, 306]
+	for i in indexOfSchedulingMails:
+		targets[i] = 1
+	for i in indexOfReSchedulingMails:
+		targets[i] = 2
+
+	allBodyMerged = []
+	for key in keys:
+		mails = mailThread[key]
+		temp = "";
+		for mail in mails:
+			if 'body' in mail and len(mail['body'])!=0:
+				indx = mail['body'].index("----") if "----" in mail['body'] else -1
+				s = MLStripper()
+				s.feed( mail['body'].replace("\n","").replace("\r","").replace("\t","")[:indx] )
+				temp += s.get_data() + ". "
+		allBodyMerged += [ temp ]
+
+	# Augmenting the schdeuling mails
+	schdeulingMails = [ \
+			'Lets meet at 2 am', \
+			'Lets meet in the afternoon',\
+			'scheduled a meeting for tomorrow',\
+			'we will meet tomorrow',\
+			'we are meeting tomorrow',\
+			'we will meet today',\
+			'we are meeting today',\
+			'would like to meet and speak to you on the 15th of September',\
+			'The monthly meeting will be on Monday, 12th December 2011',\
+			'The meetings will be on Monday',\
+			'see you at 10:00',\
+			'see you at 10',\
+			'see you in the evening',\
+			'meeting is set at 10:20 pm',\
+			'meeting on Tuesday.'
+			]
+	for m in schdeulingMails:
+		allBodyMerged += [m]
+		targets += [1]
+
+	# Augmenting the Reschdeuling mails
+	reschdeulingMailsSamples = [\
+			 'rescheduled the meeting for tomorrow'
+			 'we have tentatively rescheduled the meeting for August 20.'
+			 'we will postpone the meet',\
+			 'we will postpone the meeting',\
+			 'we will reschedule the meet',\
+			 'we will reschedule the meeting',\
+			 'We regret to inform you the August 8 meeting has been postponed.',\
+			 'I\'m afraid I have to request to reschedule our meeting of June 15.',\
+			 'This is to inform you that the board meeting that was scheduled to happen on the 12th of September has been postponed to the 15th of September due to some official reasons.  We are sorry for the inconvenience caused and would like to meet and speak with you on the 15th of September.',\
+			 'I deeply regret that I will be unable to keep our appointment scheduled at 12: 00 noon on Wednesday, 5th March 2014. Due to some urgent circumstances in my office, I have to travel to Singapore, hence my inability to attend the aforementioned meeting.I will return on 12th March 2014 and as soon as I return, I will call your office to reschedule our meeting. Probably we can meet the next day after my return and will call your office in this regard. Once again, I apologise my inability to attend the meeting.',\
+			 'Please consider my humble request for postponing my appointment date and be kind enough to permit me to join on a future date and oblige.',\
+			 'This is to inform you that our meeting for Tuesday has to be postponed.',\
+			 'This email is to let you all know that our scheduled meeting for this afternoon has been postponed.',
+			 'could you please reschedule the meeting for tomorrow',
+			 'The monthly meeting that was to take place on Monday, 12th December 2011 has been postponed because of an urgent visit that I will be making on the project site on that day during the morning hours.'
+			]
+	for m in reschdeulingMailsSamples:
+		allBodyMerged += [m]
+		targets += [2]
+
+	targets = np.array(targets)
+
+	# Tokenizing text 
+	count_vect = CountVectorizer()
+	X_train_counts = count_vect.fit_transform(allBodyMerged)
+	X_train_counts.shape
+
+	# From occurrences to frequencies
+	tfidf_transformer = TfidfTransformer()
+	X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+	X_train_tfidf.shape
+
+	# Training an SVM classifier
+	print "Training the SVM..."
+	clf = SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter=5, random_state=42).fit(X_train_tfidf, targets )
+
+	return clf, count_vect, tfidf_transformer
 
 
-targets = np.array(targets)
 
-# Tokenizing text 
-# ===============
-count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform(allBodyMerged)
-X_train_counts.shape
+if __name__ == '__main__':
+	
+	clf, count_vect, tfidf_transformer = buildMailClassifier()
 
-
-
-# From occurrences to frequencies
-# ===============================
-tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-X_train_tfidf.shape
-
-
-
-# Training an SVM classifier
-print "Training the SVM..."
-clf = SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter=5, random_state=42).fit(X_train_tfidf, targets )
-
-
-# Testing
-print "Testing the model..."
-print "Enter the mail content:"
-while True:
-	str = raw_input();
-	docs_new = [str]
-	X_new_counts = count_vect.transform(docs_new)
-	X_new_tfidf = tfidf_transformer.transform(X_new_counts)
-	predicted = clf.predict(X_new_tfidf)
-	print categories[predicted]
+	# Testing
+	print "Testing the model..."
+	print "Enter the mail content:"
+	while True:
+		str = raw_input();
+		docs_new = [str]
+		X_new_counts = count_vect.transform(docs_new)
+		X_new_tfidf = tfidf_transformer.transform(X_new_counts)
+		predicted = clf.predict(X_new_tfidf)
+		print categories[predicted]
