3fefc99bb510c8635db795df107256a5469c6f10
Prabhakar Bikkaneti <prabhakar.9885@gmail.com>
2016-12-16 14:09:39 +0530

    Added code for finding the Location via NER

--- Driver.py
+++ Driver.py
@@ -5,13 +5,18 @@ python Driver.py RunOnDump	# Run on enron mail data
 python Driver.py 			# Run in interactive mode
 """
 
-
 import Mail_Classifier
 import Date_Classifier
-import nltk
+import nltk, re
 import pickle as p
 from HTMLParser import HTMLParser
-import traceback, sys
+import traceback, sys, timeit
+from glob import iglob
+from nltk.tag import StanfordNERTagger
+from nltk.internals import find_jars_within_path
+
+
+start = timeit.default_timer()
 
 
 
@@ -27,6 +32,27 @@ class MLStripper(HTMLParser):
         self.fed.append(d)
     def get_data(self):
         return ''.join(self.fed)
+
+
+def doNER( text ):
+	st = StanfordNERTagger('english.muc.7class.caseless.distsim.crf.ser.gz')
+	stanford_dir = st._stanford_jar[0].rpartition('/')[0]
+	stanford_jars = find_jars_within_path(stanford_dir)
+	st._stanford_jar = ':'.join(stanford_jars)
+	tokenizer='[, \n\r:;\'\"-<>(){}\[\]/!=]'
+	words=re.split(tokenizer,text)
+	ner=st.tag(words)
+	for i in xrange(len(ner)):
+		return ner
+
+
+def getLocation( text ):
+	locations = set()
+	tps = doNER( s.get_data() )
+	for tp in tps:
+		if tp[1]=="LOCATION":
+			locations.add( (tp[0]).lower() )
+	return locations
 
 
 def getDateTime( mailBody ):
@@ -83,43 +109,63 @@ def mergedText( dateTime ):
 
 
 if len(sys.argv)==2 and sys.argv[1]=="RunOnDump":
+	i = 0
+	schedulingMails = []
+	reSchedulingMails = []
 	print "Loading the dump..."
 	mails = p.load( open("MailDataenron_mail_20150507.dump", "rb") )
+	# mails = p.load( open("./100Mails.dump", "rb") )
 	for mail in mails:
-		print "#"*20
+		
+		if i==100:
+			break
 		try:
 			indx = mail['body'].index("----") if "----" in mail['body'] else -1
 			s = MLStripper()
-			s.feed( mail['body'].replace("\n","").replace("\r","").replace("\t","")[:indx] )
+			s.feed( mail['body'].replace("\n"," ").replace("\r"," ").replace("\t"," ")[:indx] )
 			docs_new = [ s.get_data() ]
 			X_new_counts = mcount_vect.transform(docs_new)
 			X_new_tfidf = mtfidf_transformer.transform(X_new_counts)
 			predicted = mclf.predict(X_new_tfidf)[0]
 			if predicted==0:
+				continue;
 				print "Not a Scheduling mail"
-				print docs_new[0]
 			elif predicted==1:
+				print "#"*20
+				print "Actual date-time: ", mail['Date']
+				i += 1
 				print "Scheduling mail"
 				dateTime = getDateTime( docs_new[0] )
 				if dateTime == "DateTime can't be extracted.":
-					print dateTime
+					print "Extracted date-time: ", dateTime
 				else:
-					print mergedText( dateTime )
-				print docs_new[0]
+					print "Extracted date-time: ", mergedText( dateTime )
+				location = getLocation( docs_new[0] )
+				print "Location: ", "; ".join( location)
 			elif predicted==2:
+				print "#"*20
+				print "Actual date-time: ", mail['Date']
+				i += 1
 				print "Re-Scheduling mail"
 				dateTime = getDateTime( docs_new[0] )			
 				if dateTime == "DateTime can't be extracted.":
-					print dateTime
+					print "Extracted date-time: ", dateTime
 				else:
-					print mergedText( dateTime )
-				print docs_new[0]
+					print "Extracted date-time: ", mergedText( dateTime )
+				location = getLocation( docs_new[0] )
+				print "Location: ", "; ".join( location )
+			print "Participants: ", "; ".join( [mail["From"]] + mail["X-To"] )
+			print "Subject:\n", mail['Subject'] if 'Subject' in mail else ""
+			print "Body:\n", docs_new[0]
 		except Exception, err:
 			print "Exception: "
 			print err
 			print mail["Date"];
 			print mail["Subject"];
 			print traceback.print_exc()
+
+		# p.dump( schedulingMails, open("schedulingMails.dump","wb+") )
+		# p.dump( reSchedulingMails, open("reSchedulingMails.dump","wb+") )
 else:
 	while True:
 		print "\nEnter the mail content:"
@@ -140,3 +186,5 @@ else:
 			dateTime = getDateTime(str)
 			print dateTime
 			print mergedText( dateTime )
+
+print "Runtime: ", timeit.default_timer() - start
